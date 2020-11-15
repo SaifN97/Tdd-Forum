5cd3b67d743e9e6bb40dd8d2a467139a35367445
SaifN97 <saifnarpali@gmail.com>
2020-11-15 13:14:17 +0530

    Exception handling conundrum

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,21 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Exception;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->disableExceptionHandling();
+    }
 
     protected function signIn($user = null)
     {
@@ -14,6 +24,32 @@ abstract class TestCase extends BaseTestCase
 
         $this->actingAs($user);
 
+        return $this;
+    }
+
+    public function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+
+        $this->app->instance(ExceptionHandler::class, new class extends Handler
+        {
+            public function __construct()
+            {
+            }
+            public function report(Exception $exception)
+            {
+            }
+            public function render($request, Exception $exception)
+            {
+                throw $exception;
+            }
+        });
+    }
+
+
+    public function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
         return $this;
     }
 }
