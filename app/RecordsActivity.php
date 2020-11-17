069790f83b309933560c66cd5efefbb8fc253008
SaifN97 <saifnarpali@gmail.com>
2020-11-17 17:08:25 +0530

    Fixed deleting activity bug

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -16,6 +16,11 @@ trait RecordsActivity
                 $model->recordActivity($event);
             });
         }
+
+
+        static::deleting(function ($model) {
+            $model->activity()->delete();
+        });
     }
 
 
