c91331c8fb7a2e8fb2cc47a629839d2351fd5302
SaifN97 <saifnarpali@gmail.com>
2020-11-28 19:37:51 +0530

    Added feature threads have unique slugs

--- app/Thread.php
+++ app/Thread.php
@@ -33,7 +33,7 @@ class Thread extends Model
 
     public function path()
     {
-        return "/threads/{$this->channel->slug}/{$this->id}";
+        return "/threads/{$this->channel->slug}/{$this->slug}";
     }
 
     public function creator()
@@ -99,5 +99,10 @@ class Thread extends Model
         $key = $user->visitedThreadCacheKey($this);
 
         return $this->updated_at > cache($key);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
