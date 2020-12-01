5dfbf501f2c04493bd207f1d01b6f947027379a7
SaifN97 <saifnarpali@gmail.com>
2020-12-01 20:20:32 +0530

    Added frontend for locking a thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-    <thread-view :initial-replies-count="{{ $thread->replies_count }}" inline-template>
+    <thread-view :data-replies-count="{{ $thread->replies_count }}" :data-locked="{{ $thread->locked }}" inline-template>
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
@@ -50,8 +50,11 @@
                             </p>
 
                             <p>
-                                <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}">
+                                <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}" v-if="signedIn">
                                 </subscribe-button>
+
+                                <button class="btn btn-default" v-if="authorize('isAdmin') && ! locked"
+                                    @click="locked = true">Lock</button>
                             </p>
                         </div>
                     </div>
