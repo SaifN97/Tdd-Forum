59456d398f2dfe48f18c7a1c340890867d2fa7d2
SaifN97 <saifnarpali@gmail.com>
2020-12-02 18:07:11 +0530

    Added recaptcha for validation

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,53 +1,63 @@
 @extends('layouts.app')
 
+@section('header')
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+@stop
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Create a New Thread</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create a New Thread</div>
 
-                <div class="panel-body">
-                  <form action="/threads" method="POST">
-                      {{ csrf_field() }}
+                    <div class="panel-body">
+                        <form action="/threads" method="POST">
+                            {{ csrf_field() }}
 
-                    <div class="form-group">
-                        <label for="channel_id">Choose a Channel:</label>
-                        <select class="form-control" name="channel_id" id="channel_id" required>
-                            <option value="">Choose one...</option>
-                            @foreach ($channels as $channel)
-                                <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected': '' }}>
-                                    {{ $channel->name }}
-                                </option>
-                            @endforeach
-                        </select>
+                            <div class="form-group">
+                                <label for="channel_id">Choose a Channel:</label>
+                                <select class="form-control" name="channel_id" id="channel_id" required>
+                                    <option value="">Choose one...</option>
+                                    @foreach ($channels as $channel)
+                                        <option value="{{ $channel->id }}"
+                                            {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                            {{ $channel->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input name="title" type="text" id="title" value="{{ old('title') }}" class="form-control"
+                                    required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="body">Body</label>
+                                <textarea name="body" id="body" class="form-control" rows="8"
+                                    required>{{ old('body') }}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <div class="g-recaptcha" data-sitekey="6LdxKfYZAAAAAHK4BNiNIC3C4hjVK5AwkRrUuRzw"></div>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+
+                            @if (count($errors))
+                                <ul class="alert alert-danger">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </form>
                     </div>
-
-                      <div class="form-group">
-                          <label for="title">Title</label>
-                          <input name="title" type="text" id="title" value="{{ old('title') }}" class="form-control" required>
-                      </div>
-
-                      <div class="form-group">
-                          <label for="body">Body</label>
-                          <textarea name="body" id="body" class="form-control" rows="8" required>{{ old('body') }}</textarea>
-                      </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Publish</button>
-                        </div>
-
-                  @if (count($errors))
-                      <ul class="alert alert-danger">
-                          @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                          @endforeach
-                      </ul>
-                  @endif
-                  </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
