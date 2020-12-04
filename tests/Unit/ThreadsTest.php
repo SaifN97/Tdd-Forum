469cd984435d374ec5c8e88811a995bf5800c66b
SaifN97 <saifnarpali@gmail.com>
2020-12-04 20:50:05 +0530

    Added sanitization for all inputs

--- tests/Unit/ThreadsTest.php
+++ tests/Unit/ThreadsTest.php
@@ -149,4 +149,12 @@ class ThreadsTest extends TestCase
             $this->assertFalse($thread->hasUpdatesFor($user));
         });
     }
+
+    /** @test */
+    public function a_threads_body_is_sanitized_automatically()
+    {
+        $thread = make('App\Thread', ['body' => '<script>alert("bad")</script><p>This is okay.</p>']);
+
+        $this->assertEquals("<p>This is okay.</p>", $thread->body);
+    }
 }
